3274cfd21caa124b6029316ef5a65adebe8d54d3
ninonvt9 <delongdal@outlook.com>
2017-12-27 13:32:16 +0800

    修改build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 ﻿rm JCalc*.java
 rm *.tokens
 rm *.class
-#antlr4 -no-listener -visitor JCalc.g4
-#javac JCalc*.java MainVisit.java
-#.\test.ps1
+antlr4 -no-listener -visitor JCalc.g4
+javac jcalc*.java mainvisit.java
+.\test.ps1
